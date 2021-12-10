08b19ac0edd6e0bfaccf2f2714c5be3fabfff58d
Tanbir Ahmed Mazumdar <46240243+tanbir-create@users.noreply.github.com>
2021-12-10 22:54:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -116,8 +116,8 @@ let createSlick = () => {
             {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 // adaptiveHeight: true,
             },
             },
